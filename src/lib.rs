a9447b55962abfac271c6b4c4cbe8682260e1467
Max Heller <max.a.heller@gmail.com>
2024-06-24 09:08:53 -0400

    add test for remote redirect

--- src/lib.rs
+++ src/lib.rs
@@ -1359,6 +1359,7 @@ output-file = "/dev/null"
 to = "markdown"
 
 [output.html.redirect]
+"/appendices/bibliography.html" = "https://rustc-dev-guide.rust-lang.org/appendix/bibliography.html"
 "/foo/bar.html" = "../new-bar.html"
 "/new-bar.html" = "new-new-bar.html"
         "#;
@@ -1366,7 +1367,11 @@ to = "markdown"
             .max_log_level(tracing::Level::DEBUG)
             .init()
             .mdbook_config(mdbook::Config::from_str(cfg).unwrap())
-            .chapter(Chapter::new("", "[bar](foo/bar.md)", "index.md"))
+            .chapter(Chapter::new(
+                "",
+                "[bar](foo/bar.md)\n[bib](appendices/bibliography.html)",
+                "index.md",
+            ))
             .chapter(Chapter::new("", "", "new-new-bar.md"))
             .build();
         insta::assert_snapshot!(output, @r###"
@@ -1375,15 +1380,19 @@ to = "markdown"
         │ DEBUG mdbook::book: Running the links preprocessor.    
         │  INFO mdbook::book: Running the pandoc backend    
         │  INFO mdbook_pandoc: Processing redirects in [output.html.redirect]    
+        │ DEBUG mdbook_pandoc::preprocess: Processing redirect: /appendices/bibliography.html => https://rustc-dev-guide.rust-lang.org/appendix/bibliography.html    
         │ DEBUG mdbook_pandoc::preprocess: Processing redirect: /foo/bar.html => ../new-bar.html    
         │ DEBUG mdbook_pandoc::preprocess: Processing redirect: /new-bar.html => new-new-bar.html    
+        │ DEBUG mdbook_pandoc::preprocess: Registered redirect: book/test/src/appendices/bibliography.html => https://rustc-dev-guide.rust-lang.org/appendix/bibliography.html    
         │ DEBUG mdbook_pandoc::preprocess: Registered redirect: book/test/src/foo/bar.html => book/test/src/new-bar.html    
         │ DEBUG mdbook_pandoc::preprocess: Registered redirect: book/test/src/new-bar.html => book/test/src/new-new-bar.md    
         │ DEBUG mdbook_pandoc::pandoc::renderer: Running pandoc    
         │  INFO mdbook_pandoc::pandoc::renderer: Wrote output to /dev/null    
+        ├─ test/src/appendices/bibliography.html
         ├─ test/src/foo/bar.html
         ├─ test/src/index.md
         │ [bar](book/test/src/new-new-bar.md)
+        │ [bib](https://rustc-dev-guide.rust-lang.org/appendix/bibliography.html)
         ├─ test/src/new-bar.html
         ├─ test/src/new-new-bar.md
         "###)
