d50652624486830ea173519c820a14f0b1390f1d
Max Heller <max.a.heller@gmail.com>
2025-01-12 16:55:21 -0500

    fix: fix LuaLaTeX rendering of left-to-right text in right-to-left books (#144)
    Fixes errors like:
    ```
    Error producing PDF.
    ! Undefined control sequence.
    l.279 ...اوقات زبان Rust را با C و \LR
    ```

--- src/lib.rs
+++ src/lib.rs
@@ -2090,6 +2090,39 @@ include-in-header = ["file-in-root"]
         ");
     }
 
+    #[test]
+    #[ignore]
+    fn right_to_left_fonts_lualatex() {
+        let cfg = r#"
+[book]
+language = "fa"
+
+[output.pandoc.profile.pdf]
+output-file = "book.pdf"
+pdf-engine = "lualatex"
+
+[output.pandoc.profile.pdf.variables]
+mainfont = "Noto Naskh Arabic"
+mainfontfallback = [
+  "NotoSerif:",
+]
+        "#;
+        let output = MDBook::init()
+            .mdbook_config(cfg.parse().unwrap())
+            .chapter(Chapter::new("", "<span dir=ltr>C++</span>", "chapter.md"))
+            .build();
+        insta::assert_snapshot!(output, @r#"
+        ├─ log output
+        │  INFO mdbook::book: Running the pandoc backend    
+        │  INFO mdbook_pandoc::pandoc::renderer: Running pandoc    
+        │  INFO mdbook_pandoc::pandoc::renderer: Wrote output to book/pdf/book.pdf    
+        ├─ pdf/book.pdf
+        │ <INVALID UTF8>
+        ├─ pdf/src/chapter.md
+        │ [Para [Span ("", [], [("dir", "ltr")]) [Str "C++"]]]
+        "#);
+    }
+
     static BOOKS: Lazy<PathBuf> = Lazy::new(|| Path::new(env!("CARGO_MANIFEST_DIR")).join("books"));
 
     #[test]
